02313e40408bb8d326d72f93fbf7ae977f588ec5
seo1v <minseoeum14@gmail.com>
2025-03-31 15:10:25 +0900

    week2
    2주차 실습 파일 업로드

---
+++ mango/week2/2nd_Practice/2nd_Practice/MovieModel.swift
@@ -0,0 +1,16 @@
+//
+//  MovieModel.swift
+//  2nd_Practice
+//
+//  Created by 엄민서 on 3/31/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct MovieModel {
+    let movieImage: Image
+    let movieName: String
+    let movieLike: Int
+    let movieReserCount: Double 
+}
---
+++ mango/week2/2nd_Practice/2nd_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2nd_Practice
+//
+//  Created by 엄민서 on 3/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
