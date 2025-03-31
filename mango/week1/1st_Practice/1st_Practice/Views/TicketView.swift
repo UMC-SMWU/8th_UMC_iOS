d537dcb8986393552ce4e66e391df1933ece8f51
seo1v <minseoeum14@gmail.com>
2025-03-31 14:57:26 +0900

    week1
    1주차 실습 파일 업로드

---
+++ mango/week1/1st_Practice/1st_Practice/Views/TicketView.swift
@@ -0,0 +1,67 @@
+//
+//  TicketView.swift
+//  1st_Practice
+//
+//  Created by 엄민서 on 3/20/25.
+//
+
+import SwiftUI
+
+struct TicketView: View {
+    var body: some View {
+        ZStack {
+            Image(.background)
+            
+            VStack {
+                
+                Spacer().frame(height: 111)
+                
+                mainTitleGroup
+                
+                Spacer().frame(height: 134)
+                
+                mainBottomGroup
+            }
+        }
+        .padding()
+    }
+    
+    /// 상단 Title VStack
+    private var mainTitleGroup: some View {
+        VStack {
+            Group {
+                Text("마이펫의 이중생활2")
+                    .font(.PretendardBold30)
+                    .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+                Text("본인 + 동반 1인")
+                    .font(.PretendardLight16)
+                Text("30,100원")
+                    .font(.PretendardBold24)
+            }
+            .foregroundStyle(Color.white)
+        }
+        .frame(height: 84)
+    }
+    
+    /// 하단 VStack
+    private var mainBottomGroup: some View {
+        Button(action: {
+            print("Hello")
+        }, label: {
+            VStack {
+                Image(systemName: "chevron.up")
+                    .resizable()
+                    .frame(width: 18, height: 12)
+                    .foregroundStyle(Color.white)
+                Text("예매하기")
+                    .font(.PretendardSemiBold18)
+                    .foregroundStyle(Color.white)
+            }
+            .frame(width: 63, height: 40)
+        })
+    }
+}
+
+#Preview {
+    TicketView()
+}
