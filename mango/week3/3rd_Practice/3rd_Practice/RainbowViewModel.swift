f5961cdeda62ed9bce79a7c2939cb6bcc79fc845
seo1v <minseoeum14@gmail.com>
2025-03-31 15:28:41 +0900

    week3
    3주차 파일 업로드

---
+++ mango/week3/3rd_Practice/3rd_Practice/RainbowViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  RainbowViewModel.swift
+//  3rd_Practice
+//
+//  Created by 엄민서 on 3/31/25.
+//
+
+import Foundation
+import SwiftUI
+
+@Observable
+class RainbowViewModel {
+    var selectedRainbowModel: RainbowModel?
+    var appleLogoColor: Color?
+}
+
