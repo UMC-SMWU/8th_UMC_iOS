64639ca0b0fad4176e00306a1f7f6e5b37542524
seo1v <minseoeum14@gmail.com>
2025-04-05 08:55:09 +0900

    [Feat(망고/엄민서)] 4주차 실습
    4주차 실습 파일 업로드

---
+++ mango/week4/4th_Practice/4th_Practice/ReceiptSegment.swift
@@ -0,0 +1,33 @@
+//
+//  ReceiptSegment.swift
+//  4th_Practice
+//
+//  Created by 엄민서 on 4/5/25.
+//
+
+import Foundation
+
+enum ReceiptSegment: Int, CaseIterable, Identifiable {
+    case first
+    case second
+    
+    var id: Int { rawValue }
+    
+    var title: String {
+        switch self {
+        case .first:
+            return "첫 번째"
+        case .second:
+            return "두 번째"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .first:
+            return "first"
+        case .second:
+            return "second"
+        }
+    }
+}
---
+++ mango/week4/4th_Practice/4th_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4th_Practice
+//
+//  Created by 엄민서 on 4/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
