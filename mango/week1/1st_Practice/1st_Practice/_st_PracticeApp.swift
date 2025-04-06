1481dc8cf9934111f18b39f2353dad533783f757
seo1v <minseoeum14@gmail.com>
2025-04-07 06:11:29 +0900

    [Feat(망고/엄민서)] 실습
    실습 파일 업로드

---
+++ mango/week1/1st_Practice/1st_Practice/_st_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _st_PracticeApp.swift
+//  1st_Practice
+//
+//  Created by 엄민서 on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct _st_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TicketView()
+        }
+    }
+}
---
+++ mango/week3/3rd_Practice/3rd_Practice/_rd_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _rd_PracticeApp.swift
+//  3rd_Practice
+//
+//  Created by 엄민서 on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct _rd_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
