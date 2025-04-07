df403de78bcbc1b00bb8e26f37fb7b51756836d2
seo1v <minseoeum14@gmail.com>
2025-04-07 23:11:54 +0900

    Revert "[Feat(망고/엄민서)] CustomNavBar 수정"
    This reverts commit 60c8619570248e4031b4891da14b199391cece1f.

--- mango/week4/Starbucks/Starbucks/Common/CommonView/CustomNavBar.swift
+++ mango/week4/Starbucks/Starbucks/Common/CommonView/CustomNavBar.swift
@@ -9,47 +9,30 @@ import SwiftUI
 
 struct CustomNavBar: View {
     let viewTitle: String
-    var showBackButton: Bool = true
-    var showPlusButton: Bool = false
-    var backAction: (() -> Void)? = nil
-    var plusAction: (() -> Void)? = nil
-
+    let backButton: (() -> Void)
+        
+    init(viewTitle: String, backButton: @escaping (() -> Void)) {
+        self.viewTitle = viewTitle
+        self.backButton = backButton
+    }
+    
     var body: some View {
         HStack {
-            if showBackButton {
-                Button(action: {
-                    backAction?()
-                }) {
-                    Image("backbutton")
-                        .resizable()
-                        .foregroundStyle(.black)
-                        .frame(width: 10, height: 16)
-                }
-            } else {
-                Spacer().frame(width: 16)
+            Button(action : {
+                backButton()
+            }) {
+                Image("backbutton")
+                    .foregroundStyle(.black)
+                    .frame(width: 24, height: 24)
             }
             Spacer()
             Text(viewTitle)
                 .font(.mainTextMedium16)
             Spacer()
-
-            if showPlusButton {
-                Button(action: {
-                    plusAction?()
-                }) {
-                    Image("plusbutton")
-                        .resizable()
-                        .foregroundStyle(.black)
-                        .frame(width: 16, height: 16)
-                }
-            } else {
-                Spacer().frame(width: 16)
-            }
+            Rectangle()
+                .fill(.clear)
+                .frame(width: 24, height: 24)
         }
-        .padding(.horizontal, 15)
-        .padding(.top, 15)
-        .padding(.bottom, 22)
-        .background(Color.white00)
-        .frame(maxWidth: .infinity, minHeight: 96)
+        .padding(.horizontal, 19)
     }
 }
