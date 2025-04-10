bbe9f98bde3693ac291ae2a304b8e3c34df27174
seo1v <minseoeum14@gmail.com>
2025-04-11 02:07:31 +0900

    [Feat(망고/엄민서)] 5주차 실습
    5주차 실습 파일 업로드

---
+++ mango/week5/5th_Practice/5th_Practice/_th_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _th_PracticeApp.swift
+//  5th_Practice
+//
+//  Created by 엄민서 on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct _th_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            JSONParsingView()
+        }
+    }
+}
