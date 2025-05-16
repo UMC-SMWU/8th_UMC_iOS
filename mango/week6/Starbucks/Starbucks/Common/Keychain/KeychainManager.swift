417cfd8306bd9cd11f1efc88617c4c3cf2f3bca8
seo1v <minseoeum14@gmail.com>
2025-05-16 21:25:17 +0900

    [Feat(망고/엄민서)] 키체인

---
+++ mango/week6/Starbucks/Starbucks/Common/Keychain/KeychainManager.swift
@@ -0,0 +1,46 @@
+//
+//  KeychainManager.swift
+//  Starbucks
+//
+//  Created by 엄민서 on 5/12/25.
+//
+
+import Foundation
+import Security
+
+enum KeychainManager {
+    static func save(_ key: String, value: String) {
+        if let data = value.data(using: .utf8) {
+            let query: [String: Any] = [
+                kSecClass as String: kSecClassGenericPassword,
+                kSecAttrAccount as String: key,
+                kSecValueData as String: data
+            ]
+            SecItemDelete(query as CFDictionary)
+            SecItemAdd(query as CFDictionary, nil)
+        }
+    }
+
+    static func load(_ key: String) -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        var result: AnyObject?
+        if SecItemCopyMatching(query as CFDictionary, &result) == noErr,
+           let data = result as? Data {
+            return String(data: data, encoding: .utf8)
+        }
+        return nil
+    }
+
+    static func delete(_ key: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key
+        ]
+        SecItemDelete(query as CFDictionary)
+    }
+}
